a3099cfd02dcff8d54cfac4dbf0e2ff1df4de598
ttenneBnaeS <seanerbennett@gmail.com>
2026-07-18 14:55:32 -0500

    Agent search with langchain_tavily search

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_anthropic import ChatAnthropic
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 import os
 load_dotenv()
 
-tavily_client = TavilyClient()
 
-@tool
-def search(query: str) -> str:
-    """Search the web for information"""
-    response = tavily_client.search(query)
-    return response
 
 llm = ChatAnthropic(model="claude-sonnet-5")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
